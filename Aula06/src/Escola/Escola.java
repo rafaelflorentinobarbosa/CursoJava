767dc23f5400a1ccd1c4a032898339a3fb67664f
Rafael Florentino <rafael.florentino.barbosa@gmail.com>
2024-06-13 15:17:34 -0300

    ajustes identação escola

--- Aula06/src/Escola/Escola.java
+++ Aula06/src/Escola/Escola.java
@@ -12,21 +12,21 @@ public class Escola {
         //Aluno aluno1 = new Aluno("Pedro",19,9.5,9.3);
         
         System.out.println("Digite o nome do aluno: ");
-        nome =entrada.nextLine();
+        nome = entrada.nextLine();
         System.out.println("Digite a idade do aluno: ");
-        idade= entrada.nextInt();
+        idade = entrada.nextInt();
         System.out.println("Digite a primeira nota: ");
-        nota1= entrada.nextDouble();
+        nota1 = entrada.nextDouble();
         System.out.println("Digite a segunda nota: ");
-        nota2= entrada.nextDouble();
+        nota2 = entrada.nextDouble();
 
-        Aluno aluno1 = new Aluno(nome,idade,nota1,nota2);
+        Aluno aluno1 = new Aluno(nome, idade, nota1, nota2);
 
         System.out.println("O nome do aluno é: "+aluno1.getNome());
         System.out.println("A idade do aluno é: "+aluno1.getIdade());
 
         aluno1.media();
-
+        
         entrada.close();
 
     }
