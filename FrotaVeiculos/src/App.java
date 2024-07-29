7f7e79c6c5b7f6ab90f4f70f27e5c080650f6827
Rafael Florentino <rafael.florentino.barbosa@gmail.com>
2024-07-29 14:08:34 -0300

    versão nova

--- FrotaVeiculos/src/App.java
+++ FrotaVeiculos/src/App.java
@@ -1,12 +1,13 @@
-//import javax.swing.JFrame;
+import view.MenuPrincipal;
 
 public class App {
     public static void main(String[] args) throws Exception {
+<<<<<<< Updated upstream:FrotaVeiculos/src/App.java
 
         //  Tela do menu de opçôes do sistema
+=======
+>>>>>>> Stashed changes:RafaelFlorentinoTrabalhoJava/src/App.java
         @SuppressWarnings("unused")
         MenuPrincipal menuPrincipal = new MenuPrincipal();
-
     }
 }
-
