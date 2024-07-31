00b5caad4a16ec9f5972409d01b4e4f4e42a41f4
Rafael Florentino <rafael.florentino.barbosa@gmail.com>
2024-07-31 14:38:59 -0300

    ajuste menu principal

--- RafaelFlorentinoTrabalhoJava/src/view/MenuPrincipal.java
+++ RafaelFlorentinoTrabalhoJava/src/view/MenuPrincipal.java
@@ -23,19 +23,24 @@ public class MenuPrincipal extends JFrame {
 
     public MenuPrincipal() {
         
-        listaVeiculos.add(new Veiculo("Ford", "Fiesta", 2015, 2016, 1.6, 50, "Gasolina", "Preto", "ABC1234", "1234567890"));
-        listaVeiculos.add(new Veiculo("Chevrolet", "Onix", 2018, 2019, 1.4, 45, "Flex", "Branco", "DEF5678", "0987654321"));
-        listaVeiculos.add(new Veiculo("Volkswagen", "Golf", 2020, 2021, 2.0, 55, "Gasolina", "Vermelho", "GHI9012", "1122334455"));
-        listaVeiculos.add(new Veiculo("Honda", "Civic", 2017, 2018, 1.8, 47, "Gasolina", "Cinza", "JKL3456", "2233445566"));
-        listaVeiculos.add(new Veiculo("Toyota", "Corolla", 2019, 2020, 2.0, 50, "Flex", "Azul", "MNO7890", "3344556677"));
-        listaDespesas.add(new Despesa("Multa velocidade", 150.00f, "ABC1234", "Multa", "estava 80km/h em uma pista de 60"));
-        listaDespesas.add(new Despesa("Ipva", 1200.00f, "ABC1234", "Imposto", "Parcela unica"));
-        listaDespesas.add(new Despesa("Manutenção do Carro", 350.00f, "MNO7890", "Manutenção", "Troca de óleo e filtro"));
-        listaDespesas.add(new Despesa("Seguro do Carro", 500.00f, "JKL3456", "Outros", "Mensalidade do seguro do carro"));
-        listaDespesas.add(new Despesa("Multa velocidade", 150.00f, "GHI9012", "Multa", "estava 80km/h em uma pista de 60"));
-        listaDespesas.add(new Despesa("Ipva", 1200.00f, "GHI9012", "Imposto", "Parcela unica"));
-        listaDespesas.add(new Despesa("Manutenção do Carro", 350.00f, "DEF5678", "Manutenção", "Troca de óleo e filtro"));
-        listaDespesas.add(new Despesa("Seguro do Carro", 500.00f, "DEF5678", "Outros", "Mensalidade do seguro do carro"));
+        // Povoando o programa
+
+        if(listaVeiculos.isEmpty()){
+            listaVeiculos.add(new Veiculo("Ford", "Fiesta", 2015, 2016, 1.6, 50, "Gasolina", "Preto", "ABC1234", "1234567890"));
+            listaVeiculos.add(new Veiculo("Chevrolet", "Onix", 2018, 2019, 1.4, 45, "Flex", "Branco", "DEF5678", "0987654321"));
+            listaVeiculos.add(new Veiculo("Volkswagen", "Golf", 2020, 2021, 2.0, 55, "Gasolina", "Vermelho", "GHI9012", "1122334455"));
+            listaVeiculos.add(new Veiculo("Honda", "Civic", 2017, 2018, 1.8, 47, "Gasolina", "Cinza", "JKL3456", "2233445566"));
+            listaVeiculos.add(new Veiculo("Toyota", "Corolla", 2019, 2020, 2.0, 50, "Flex", "Azul", "MNO7890", "3344556677"));
+            
+            listaDespesas.add(new Despesa("Multa velocidade", 150.00f, "ABC1234", "Multa", "estava 80km/h em uma pista de 60"));
+            listaDespesas.add(new Despesa("Ipva", 1200.00f, "ABC1234", "Imposto", "Parcela unica"));
+            listaDespesas.add(new Despesa("Manutenção do Carro", 350.00f, "MNO7890", "Manutenção", "Troca pastilha freio, luz faro queimado"));
+            listaDespesas.add(new Despesa("Seguro do Carro", 500.00f, "JKL3456", "Outros", "Mensalidade do seguro do carro"));
+            listaDespesas.add(new Despesa("Multa estacionamento", 150.00f, "GHI9012", "Multa", "estacionou em frente a garajem"));
+            listaDespesas.add(new Despesa("DPVAT", 1200.00f, "GHI9012", "Imposto", "referente a 2024"));
+            listaDespesas.add(new Despesa("Troca oleo", 350.00f, "DEF5678", "Manutenção", "Troca de óleo e filtro"));
+            listaDespesas.add(new Despesa("Seguro do Carro", 500.00f, "DEF5678", "Outros", "Mensalidade do seguro do carro"));
+        }
 
         // Configurações do Frame
 
