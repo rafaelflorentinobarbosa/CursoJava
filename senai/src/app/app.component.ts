d646610f868b81dedc8dc06af8508b7d43cfc252
Rafael Florentino <rafael.florentino.barbosa@gmail.com>
2024-08-08 17:45:47 -0300

    angular formulario

--- senai/src/app/app.component.ts
+++ senai/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+      
+  }
+
   title = 'senai';
+  result = 0;
+
+  getHelloMessage(): string {
+    console.log("Hello XD")
+    alert('OLá Mundo')
+    return 'Hello World';
+  }
+
+  cadastro(): string {
+    console.log()
+    return 'Hello World';
+  }
+
+  soma(a: String, b: String){
+    let numA = Number(a); 
+    let numB = Number(b);
+
+    console.log(numA + numB)
+    return numA + numB;
+  }
+
+  
 }
