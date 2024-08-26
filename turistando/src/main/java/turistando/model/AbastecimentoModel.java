2799747cb5408e3db263a8e7d917e1ff53e32c38
Rafael Florentino <rafael.florentino.barbosa@gmail.com>
2024-08-26 17:51:06 -0300

    ajustes enum

--- turistando/src/main/java/turistando/model/AbastecimentoModel.java
+++ turistando/src/main/java/turistando/model/AbastecimentoModel.java
@@ -3,6 +3,8 @@ package turistando.model;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +12,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import turistando.enuns.CombustivelEnum;
 
 @Entity
 @Table(name = "tb_abastecimento")
@@ -32,8 +35,8 @@ public class AbastecimentoModel implements Serializable {
     private double quilometragem;
 
     @NotNull(message = "Tipo de combustível não pode ser nulo")
-    @Pattern(regexp = "Gasolina|Aalcool|Diesel|GNV", message = "Tipo de combustível inválido")
-    private String tipoCombustivel;  
+    @Enumerated(EnumType.STRING)
+    private CombustivelEnum tipoCombustivel;
 
     @NotNull(message = "Placa não pode ser nula")
     @Pattern(regexp = "[A-Z0-9]+", message = "Placa deve conter apenas letras e números")
@@ -63,17 +66,18 @@ public class AbastecimentoModel implements Serializable {
     public void setPlaca(String placa) {
         this.placa = placa;
     }
-    public String getTipoCombustivel() {
-        return tipoCombustivel;
-    }
-    public void setTipoCombustivel(String tipoCombustivel) {
-        this.tipoCombustivel = tipoCombustivel;
-    }
+
     public int getId() {
         return id;
     }
     public void setId(int id) {
         this.id = id;
+    }
+    public CombustivelEnum getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+    public void setTipoCombustivel(CombustivelEnum tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
     }
 
     
