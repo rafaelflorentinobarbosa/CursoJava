fa9fd469784a7efa2520df46bdf564fc00ce77b0
unknown <rafael.florentino.barbosa@gmail.com>
2024-08-27 00:42:22 -0300

    ajustes cadastrar despesas no veiculo

--- turistando/src/main/java/turistando/controller/DespesaController.java
+++ turistando/src/main/java/turistando/controller/DespesaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import turistando.model.DespesaModel;
 import turistando.repository.DespesaRepository;
+import turistando.services.DespesaServices;
 
 @RestController
 public class DespesaController {
@@ -17,10 +18,23 @@ public class DespesaController {
     @Autowired
     private DespesaRepository despesaRepository;
 
+    @Autowired
+    private DespesaServices despesaServices;
+
+    // @PostMapping("/cadastrardespesa")
+    // public DespesaModel cadastrarVeiculo(@RequestBody DespesaModel despesa) {    
+    //     return despesaRepository.save(despesa);
+    // }
+
     @PostMapping("/cadastrardespesa")
-    public DespesaModel cadastrarVeiculo(@RequestBody DespesaModel despesa) {    
-        return despesaRepository.save(despesa);
+    public DespesaModel registrarDespesa(@RequestBody DespesaModel despesa) {
+        try {
+            return despesaServices.registrarDespesa(despesa);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
+
 
     @GetMapping("/mostrardespesa")
     public List<DespesaModel> listaDespesaModel() {
